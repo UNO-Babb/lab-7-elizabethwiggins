cd5b743ed2f4fea24abb8e4a255789d3ade3a278
elizabethwiggins <ewiggins@unomaha.edu>
2025-03-06 17:49:58 +0000

    stuck

---
+++ Problem4.py
@@ -0,0 +1,20 @@
+#Problem4.py
+#Project Euler problem 7
+
+from NumberTests import isPrime
+
+
+def main():
+    range(2,10001)
+    
+    
+
+
+  
+
+          
+
+
+
+if __name__ == '__main__':
+  main()
