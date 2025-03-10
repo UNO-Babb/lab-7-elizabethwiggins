e054339b2dcf5a87883847f52bb2e97f5ebb21a0
elizabethwiggins <ewiggins@unomaha.edu>
2025-03-10 21:53:07 +0000

    idk

---
+++ Problem5.py
@@ -0,0 +1,27 @@
+#Problem4.py
+#Project Euler problem 10
+
+from NumberTests import isPrime
+
+
+def main():
+ index=0
+ for num in range(2,10):
+     list=(num)
+     print (sum(list))
+     
+ 
+  
+     
+
+    
+
+
+  
+
+          
+
+
+
+if __name__ == '__main__':
+  main()
